e326db38b87a19489350f8186968cf666965a994
Oguz Ozgen <oguzozgen@outlook.com>
2020-02-29 12:23:09 +0000

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webrtc_dotnet_core_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
